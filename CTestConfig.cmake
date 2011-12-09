5a8a6842d2f6c7c5b87d5a2a5d1cdd9ff0638194
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2011-12-09 01:22:56 +0000

    Changed to match updated CMake modules.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,3 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
 SET(CTEST_PROJECT_NAME "MaidSafe-Private")
 SET(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 SET(CTEST_DROP_METHOD "http")
