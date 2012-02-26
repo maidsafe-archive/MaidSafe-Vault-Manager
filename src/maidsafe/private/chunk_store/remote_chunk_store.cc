85cf18623af423d1129d3564923a5c7f1b63274a
Dan Schmidt <dan.schmidt@maidsafe.net>
2012-02-26 02:10:40 +0000

    Fix to use same directory for all local manager chunkstores but different for the buffered chunk stores

--- src/maidsafe/private/chunk_store/remote_chunk_store.cc
+++ src/maidsafe/private/chunk_store/remote_chunk_store.cc
@@ -583,7 +583,9 @@ std::shared_ptr<RemoteChunkStore> CreateLocalChunkStore(
     boost::asio::io_service &asio_service) {  // NOLINT (Dan)
   std::shared_ptr<BufferedChunkStore> buffered_chunk_store(
       new BufferedChunkStore(asio_service));
-  buffered_chunk_store->Init(base_dir / "buffered_chunk_store");
+  std::string buffered_chunk_store_dir("buffered_chunk_store" +
+                                       RandomAlphaNumericString(8));
+  buffered_chunk_store->Init(base_dir / buffered_chunk_store_dir);
   std::shared_ptr<chunk_actions::ChunkActionAuthority> chunk_action_authority(
       new chunk_actions::ChunkActionAuthority(buffered_chunk_store));
   std::shared_ptr<LocalChunkManager> local_chunk_manager(
