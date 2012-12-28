0a237614d413fb822d233eb02c85389cfe43916e
David Irvine <david.irvine@maidsafe.net>
2012-12-28 16:48:20 +0000

    update to data types

--- include/maidsafe/data_types/mutable_data.h
+++ include/maidsafe/data_types/mutable_data.h
@@ -14,28 +14,14 @@
 * ============================================================================
 */
 
-#ifndef MAIDSAFE_DATA_TYPES_DATA_TYPES_H_
-#define MAIDSAFE_DATA_TYPES_DATA_TYPES_H_
+#ifndef MAIDSAFE_DATA_TYPES_MUTABLE_DATA_H_
+#define MAIDSAFE_DATA_TYPES_MUTABLE_DATA_H_
 
 #include "maidsafe/common/types.h"
 #include "maidsafe/common/rsa.h"
 #include "maidsafe/common/tagged_value.h"
 
 namespace maidsafe {
-
-class ImmutableData {
- public:
-  typedef TaggedValue<Identity, struct ImmutableDataTag> name_type;
-
-  ImmutableData(const name_type& name, const NonEmptyString& content);
-  explicit ImmutableData(const NonEmptyString& serialised_data);
-  NonEmptyString Serialise() const;
-  name_type name() const;
- private:
-  void Validate();
-  NonEmptyString data_;
-  name_type name_;
-};
 
 class MutableData {
  public:
@@ -55,27 +41,7 @@ class MutableData {
   asymm::Signature signature_;
   int32_t version_;
 };
-
-class SignatureData {
- public:
-  typedef TaggedValue<Identity, struct SignatureDataTag> name_type;
-  SignatureData(const name_type& name,
-                const asymm::PublicKey& content,
-                const asymm::Signature& signature,
-                const asymm::PublicKey& validation_key);
-  explicit SignatureData(const NonEmptyString& serialised_data);
-  NonEmptyString Serialise() const;
-  name_type name() const;
-  int32_t version() const;  // use randomint32 as faster than hash
-
- private:
-  void Validate();
-  NonEmptyString data_;
-  name_type name_;
-  int32_t version_;
-};
-
 }  // namespace maidsafe
 
-#endif  // MAIDSAFE_DATA_TYPES_DATA_TYPES_H_
+#endif  // MAIDSAFE_DATA_TYPES_MUTABLE_DATA_H_
 
--- src/maidsafe/data_types/mutable_data.cc
+++ src/maidsafe/data_types/mutable_data.cc
@@ -13,7 +13,7 @@
 *
 * ============================================================================
 */
-#include "maidsafe/data_types/data_types.h"
+#include "maidsafe/data_types/mutable_data.h"
 
 #include "maidsafe/common/types.h"
 #include "maidsafe/common/crypto.h"
@@ -23,86 +23,17 @@
 
 namespace maidsafe {
 
-ImmutableData::ImmutableData(const name_type& name, const NonEmptyString& content)
-  : data_(content), name_(name) {
-    Validate();
-}
+MutableData::MutableData(const ChunkId name,
+                         const NonEmptyString content,
+                         const rsa::Signature signature) {}
 
-ImmutableData::ImmutableData(const NonEmptyString& serialised_data) : data_(), name_() {
-  data_types::proto::Content data_proto;
-  data_proto.ParseFromString(serialised_data.string());
-  // name_ = Identity(data_proto.name());
-  // data_ = NonEmptyString(data_proto.content().data());
-  Validate();
-}
+MutableData::MutableData(const NonEmptyString serialised_data) {}
 
-void ImmutableData::Validate() {
-  // if (name_ != crypto::Hash<crypto::SHA512>(data_))
-  //   ThrowError(CommonErrors::hashing_error);
-}
+MutableData::NonEmptyString Serialise() {}
 
-ImmutableData::name_type ImmutableData::name() const {
-  return name_;
-}
+MutableData::Identity name() {}
 
-NonEmptyString ImmutableData::Serialise() const {
-  data_types::proto::Content data_proto;
-//   data_types::proto::Content content_proto;
-//   data_proto.set_type(priv::data_types::proto::kDataType::ImmutableData);
-//   data_proto.set_name(name_.string());
-//   content_proto.set_data(data_.string());
-//   data_proto.mutable_content()->CopyFrom(content_proto);
-  return NonEmptyString(data_proto.SerializeAsString());
-
-}
-
-// TODO(Team) implement these!!
-//
-// class MutableData {
-//  public:
-//   MutableData(const priv::ChunkId name,
-//               const NonEmptyString content,
-//               const rsa::Signature signature);
-//   MutableData(const NonEmptyString serialised_data);
-//   NonEmptyString Serialise();
-//   Identity name();
-//   NonEmptyString version();
-//  private:
-//   bool Validate();
-//   NonEmptyString data_;
-//   Identity name_;
-// };
-//
-// class rsa::SignatureData {
-//  public:
-//   rsa::SignatureData(const priv::ChunkId name,
-//               const asymm::PublicKey content,
-//               const rsa::Signature signature);
-//   rsa::SignatureData(const NonEmptyString serialised_data);
-//   NonEmptyString Serialise();
-//   Identity name();
-//   NonEmptyString version();
-//  private:
-//   bool Validate();
-//   NonEmptyString data_;
-//   Identity name_;
-// };
-//
-// class AppendableData {
-//  public:
-//   AppendableData(const priv::ChunkId name,
-//               const NonEmptyString content,
-//               const std::vector<asymm::PublicKey> allowed,
-//               const rsa::Signature signature);
-//   AppendableData(const NonEmptyString serialised_data);
-//   NonEmptyString Serialise();
-//   Identity name();
-//   NonEmptyString version();
-//  private:
-//   bool Validate();
-//   NonEmptyString data_;
-//   Identity name_;
-// };
+MutableData::NonEmptyString version() {}
 
 }  // namespace maidsafe
 
